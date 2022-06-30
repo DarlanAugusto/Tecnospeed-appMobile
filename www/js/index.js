32a96e47c2139dec7c68b6acacaceb794487880c
Darlan Silva <darlansilv4@gmail.com>
2022-06-30 15:09:22 -0300

    Instalando template blank (limpando app)

--- www/js/index.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
