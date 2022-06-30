b7ad3f40e7ae187f610ad60117df58198380098c
Darlan Silva <darlansilv4@gmail.com>
2022-06-30 15:06:08 -0300

    Criando projeto com o cordova

---
+++ www/js/index.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
